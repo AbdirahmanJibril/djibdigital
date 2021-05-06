0ee3fad888f9cb1cbcd66a063c87ddd7e7e543c0
ABDIRAHMAN JIBRIL MEAD <abdirahmanjibril@outlook.com>
2021-05-06 19:43:30 +0100

    add ssl redirect

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ejs = require("ejs");
 const nodemailer = require("nodemailer");
 const session = require('express-session');
 const flash= require("express-flash"); 
+import sslRedirect from 'heroku-ssl-redirect';
 
 
 const port= process.env.PORT || 3000;
@@ -25,7 +26,7 @@ app.use(session({
   
 }));
 app.use(flash());
-
+app.use(sslRedirect());
 
 
 app.get('/', (req,res)=>{
